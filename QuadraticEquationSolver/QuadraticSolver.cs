f3e168afe5e79f3a40e10a54b2062d4eae72b780
Mariia Kulidzhanova <mkulidzhanova@axmor.com>
2024-10-17 17:24:07 +0300

    Add Solver class implementation

--- QuadraticEquationSolver/QuadraticSolver.cs
+++ QuadraticEquationSolver/QuadraticSolver.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace QuadraticEquationSolver
+﻿namespace QuadraticEquationSolver
 {
-    internal class QuadraticSolver
+    public class QuadraticSolver
     {
+        public double[] Solve(double a, double b, double c)
+        {
+            if (Math.Abs(a) < double.Epsilon)
+                throw new ArgumentException("Coefficient 'a' cannot be zero.");
+
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0)
+            {
+                double sqrtDiscriminant = Math.Sqrt(discriminant);
+                double root1 = (-b + sqrtDiscriminant) / (2 * a);
+                double root2 = (-b - sqrtDiscriminant) / (2 * a);
+                return new double[] { root1, root2 };
+            }
+            else if (discriminant == 0)
+            {
+                double root = -b / (2 * a);
+                return new double[] { root };
+            }
+            else
+            {
+                return new double[] { };
+            }
+        }
     }
 }
